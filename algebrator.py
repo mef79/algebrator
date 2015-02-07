0dae300dc9b9d8dd05a4d81c51268a0a75e12430
Meaghan <mef79@case.edu>
2015-02-07 01:06:03 -0500

    Creates random equation

--- algebrator.py
+++ algebrator.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask import render_template
+from random import randint
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-     return render_template('index.html')
+	a, b, c = create_equation()
+	return render_template('index.html', a=a, b=b, c=c)
+
+def create_equation():
+	a = randint(0, 10)
+	b = randint(0, 10)
+	c = randint(0, 10)
+	return a, b, c
 
 if __name__ == "__main__":
     app.run()
